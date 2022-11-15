8f44ba341d874b5f4808e2ee28a96d89a9305afa
athouba99 <athooba99@gmail.com>
2022-11-15 19:32:19 +0300

    not done yet, class need to be made

--- lib/main.dart
+++ lib/main.dart
@@ -61,55 +61,48 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  final controller = TextEditingController(); // defining a controller
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+          child: Column(
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              hintText: "enter you name",
+              prefixIcon: Icon(Icons.person), // person = persont
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                child: Text("Click here"),
+                onPressed: () {
+                  print("hello ${controller.text}");
+                  int grade = int.parse(controller.text);
+
+                  // evaluating the garde
+                  if (grade >= 90) {
+                    print("you grade is A ");
+                  } else if (grade >= 80) {
+                    print("you grade is B");
+                  } else if (grade >= 70) {
+                    print("you grade is C");
+                  } else if (grade >= 60) {
+                    print("you grade is D");
+                  } else {
+                    print("you grade is F");
+                  }
+                  controller.clear();
+                },
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )
+        ],
+      )),
     );
   }
 }
---
+++ lib/styling.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+// making this class for the styling
+class outline extends StatelessWidget{
+  const outline
+}
